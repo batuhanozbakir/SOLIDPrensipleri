ff424e27f1dca90ee05347e6f0aa0a03532246a3
Batuhan Özbakir <bozbakir06@gmail.com>
2024-11-01 14:49:03 +0300

    Add project files.

---
+++ OpenClosePrinciple/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosePrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    #region YanlisKullanim
+    //public class OdemeIslemleri
+    //{
+    //    public void Ode(string odemeTuru)
+    //    {
+    //        if (odemeTuru == "KrediKarti")
+    //        {
+    //            //Kredi kartı Odeme işlemleri
+    //        }
+    //        else if (odemeTuru == "Nakit")
+    //        {
+    //            // nakit ödeme işlemleri
+    //        }
+    //        else if (odemeTuru == "Pypal")
+    //        {
+
+    //        }
+    //        //yeni ödeme türü oldukça sınıf değiştirilmek zorunda
+    //    }
+    //} 
+    #endregion
+
+    public abstract class Odeme
+    {
+        public abstract void Ode();
+    }
+
+    public class KrediKartiOdeme : Odeme
+    {
+        public override void Ode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class NakitOdeme : Odeme
+    {
+        public override void Ode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PaypalOdeme : Odeme
+    {
+        public override void Ode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SingleResponsibilityPriciple/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPriciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    #region YanlısKullanim
+    //public class Urun
+    //{
+    //    public int Id { get; set; }
+    //    public string Ad { get; set; }
+    //    public decimal Fiyat { get; set; }
+
+    //    public void UrunEkle()
+    //    {
+    //        // ürün ekleme işlemleri
+    //    }
+
+    //    public void UrunSil()
+    //    {
+    //        // ürün silme işlemleri
+    //    }
+
+    //    public void UrunGuncelle()
+    //    {
+    //        // ürün güncelleme işlemleri
+    //    }
+
+    //    public void UrunRaporuOlustur()
+    //    {
+    //        // ürün rapor işlemleri
+    //    }
+    //}
+    #endregion
+
+    //DOĞRU KULLANIM
+
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+
+    public class UrunYonetimi
+    {
+        public void UrunEkle(Urun urun)
+        {
+
+        }
+
+        public void UrunSil(int urunId)
+        {
+
+        }
+
+        public void UrunGuncelle(Urun urun)
+        {
+
+        }
+    }
+
+    public class UrunRaporYonetimi
+    {
+        public void UrunRaporuOlustur(List<Urun>urunler)
+        {
+
+        }
+    }
+}
+
